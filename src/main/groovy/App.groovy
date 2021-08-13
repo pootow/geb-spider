531011e9220501ed8a16e817f59fe33d63eb1afb
pootow <pootow@gmail.com>
2021-08-14 03:00:38 +0800

    use Closeable to insure session removal.

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -1,17 +1,25 @@
 import geb.Browser
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
+
+class CloseableBrowser implements Closeable {
+
+    @Override
+    void close() throws IOException {
+        this.quit()
+    }
+}
 
 class App {
     static void main(String[] args) {
         def browser = new Browser(driver: new RemoteWebDriver(new URL('http://localhost:4445/wd/hub'), new ChromeOptions()))
-        browser.with {
-            go('https://www.baidu.com')
-            print $('#s-top-left a').collect {it.text() }.join()
-            quit()
+        browser.metaClass.mixin CloseableBrowser
+        (browser as Closeable).withCloseable {
+            browser.with {
+                go('https://www.baidu.com')
+                print $('#s-top-left a').collect {it.text() }.join()
+                throw new Exception()
+            }
         }
-
-
     }
 }
