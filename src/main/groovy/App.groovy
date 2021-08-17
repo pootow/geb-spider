230fd047935a7eb4fa42ded3ed430441c698a213
pootow <pootow@gmail.com>
2021-08-17 18:09:07 +0800

    change web driver to local chrome driver

--- src/main/groovy/App.groovy
+++ src/main/groovy/App.groovy
@@ -1,4 +1,5 @@
 import geb.Browser
+import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 
@@ -10,16 +11,38 @@ class CloseableBrowser implements Closeable {
     }
 }
 
+class Item {
+    boolean isAd
+    String name
+}
+
 class App {
     static void main(String[] args) {
-        def browser = new Browser(driver: new RemoteWebDriver(new URL('http://localhost:4445/wd/hub'), new ChromeOptions()))
-        browser.metaClass.mixin CloseableBrowser
-        (browser as Closeable).withCloseable {
+
+//        def browser = new Browser(driver: new RemoteWebDriver(new URL('http://localhost:4445/wd/hub'), new ChromeOptions()))
+        def browser = new Browser()
+//        browser.metaClass.mixin CloseableBrowser
+//        (browser as Closeable).withCloseable {
             browser.with {
-                go('https://www.baidu.com')
-                print $('#s-top-left a').collect {it.text() }.join()
-                throw new Exception()
+                go('https://www.taobao.com')
+
+                waitFor(60*1000) {
+                    $('.site-nav-login-info-nick').text() == 'tb734832977165'
+                }
+
+                def searchForm = $('#J_TSearchForm')
+                searchForm.q = '宠物洁齿粉'
+                assert searchForm.q == '宠物洁齿粉'
+
+                withNewWindow{ searchForm.$('.btn-search').click() } {
+                    def itemDivs = $('div.items>div.item')
+
+
+                }
+
+
+
             }
-        }
+//        }
     }
 }
---
+++ src/main/groovy/GebConfig.groovy
@@ -0,0 +1,4 @@
+import org.openqa.selenium.chrome.ChromeDriver
+
+reportsDir = "build/geb-reports"
+driver = { new ChromeDriver() }
